58a2f83edada248d1c167e8bdbca5b3af7d6597a
PabloDL <pablo.delafore@hotmail.com>
2018-05-18 11:35:11 -0300

    Eliminados Archivos duplicados

--- SUBE/SUBE/src/funciones/Funciones.java
+++
@@ -1,9 +0,0 @@
-package funciones;
-import java.util.GregorianCalendar;
-
-public class Funciones {
-
-	public static String traerFechaCorta (GregorianCalendar fechaDeNacimiento ) {
-		return fechaDeNacimiento.getTime().toString();
-	}
-}
