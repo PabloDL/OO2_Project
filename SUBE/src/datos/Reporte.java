cd78629a777aa800c06fe44918865c252abea289
PabloDL <pablo.delafore@hotmail.com>
2018-05-20 21:16:13 -0300

    arreglado error

--- SUBE/src/datos/Reporte.java
+++ SUBE/src/datos/Reporte.java
@@ -10,25 +10,13 @@ public class Reporte {
 
 	
  	public String generar(){
- 		ViajeDao dao = new ViajeDao ();
- 		String hql = this.reporteCompuesto.generar();
- 		ArrayList<Viaje> viajes = null;
- 	
- 		viajes = (ArrayList<Viaje>) dao.traerReporte(hql);
- 		
- 		return viajes;
-		//TODO completar juntar todos los reportes en un query y devolverlo
- 		
-// 		"FROM Customer AS c WHERE c.dateAdded BETWEEN :stDate AND :edDate "
-//		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//		String frmDate = format.parse(startDate);
-//		String enDate = format.parse(endDate);
-//		sessionfactory.getCurrentSession()
-//		.createQuery()
-//		.setParameter("stDate", frmDate)
-//		.setParameter("edDate", enDate)
-//		.list();
- 	
+		return this.reporteCompuesto.generar();
+	}
+		
+		
+ 	public List<Viaje> retornarReporte(String hql){
+ 		ViajeDao dao = new ViajeDao (); 	
+ 		return (ArrayList<Viaje>) dao.traerReporte(hql); 			
  	}
  		
  	
